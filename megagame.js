a066f27ba7f8edd913948672490597d0a6e0998f
michael.deselincourt <michael.deselincourt@REPLGROUP.COM>
2018-08-07 22:18:05 +0100

    Beginning work

--- megagame.js
+++ megagame.js
@@ -1,12 +1,76 @@
+
+const SETTINGS = ["Contemporary sci-fi", "1940s", "1980s", "Hard sci-fi", "High sci-fi", "Age of Piracy"];
+			
 			
 			$(document).ready(function(){
-				
+			
 				main();
 				
 				// getAndRenderJson();
 			});
 			
+			
+			
 			function main() {
+			
+				
+				var megagame = {};
+				
+				generateSetting(megagame);
+				
+				generateTeams(megagame);
+				
+				$("#output").html(describeMegagame(megagame));
+			}
+			
+			// DATA GENERATION FUNCTIONS
+			
+			function generateSetting(mg) {
+					
+					mg.setting = pick(SETTINGS);
+					
+				}
+				
+			function generateTeams(mg) {
+				
+				var coreFactions = d(3) + 1;
+				
+				mg.coreFactions = coreFactions;
+				
+			}
+			
+			// DATA TO DESCRIPTION FUNCTIONS
+			
+			function describeMegagame(mg) {
+				
+				var description = "";
+				
+				description += describeSetting(mg);
+				
+				description += "Core factions = " + mg.coreFactions;
+				
+				return description;
+				
+			}
+			
+			function describeSetting(mg) {
+				return "The game's setting is " + mg.setting + ".";
+			}
+				
+				
+				// UTILITY FUNCTIONS
+				
+				function pick(a) {
+					return a[Math.floor(Math.random() * a.length)];
+				}
+				
+				function d(n) {
+					return Math.floor(Math.random() * n) + 1;
+				}
+				
+				
+				
+				/*
 				
 				var AVBC = {
 					"currencies": [
@@ -76,6 +140,7 @@
 				
 			}
 			
+			*/
 			
 			/*
 			function getAndRenderJson() {
@@ -95,5 +160,10 @@
 				var dataObject = JSON.parse(dataString); // turn the retrieved JSON into an object
 
 				$("#output").html(JSON.stringify(dataObject.rounds[1]));
+				
+			}
+			
+			function displayObject(obj) {
+				$("#output").html(Object.stringify(obj));
 				
 			}
