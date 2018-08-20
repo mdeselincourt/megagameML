0d30635a5cc1a22088ae2d50af369d7812bdfc66
michael.deselincourt <michael.deselincourt@REPLGROUP.COM>
2018-08-20 09:08:49 +0100

    Mood, map-closure, political-operational

--- megagame.js
+++ megagame.js
@@ -13,7 +13,7 @@ const NAME_STARTS = [NAME_START_ADJECTIVES, NAME_START_IMPERATIVES];
 const NAME_END_NOUNS = ["Skies", "Nightmare", "Horizons", "Civil War", "Coup", "Thunder", "Straits", "Many"];
 
 const CORE_TEAMTYPES = [
-		{"name" : "Government", "goal" : "to enact and implement legislation. "},
+		{"name" : "Legislative", "goal" : "to enact and implement legislation. "},
 		{"name" : "Political Opposition", "goal" : "to resist or replace those currently in power by essentially non-violent means. "},
 		{"name" : "Sovereign Nation", "goal" : "to pursue their own leaders' agendas. "},
 		{"name" : "Belligerent", "goal" : "to win victory in armed conflict. "},
@@ -47,81 +47,60 @@ vertex will be the concept of "away from it"
 	// Sane limits are two axes or maybe let's say theoretically SIX vertices (though six evenly distributed concepts will be 
 rare I think!)
 	
-	
-	
 */
 	
 // Based on Coup
-const MAX_IDEOLOGY_EDGES_NUMBER = 6;
+const IDEOLOGIES_NUMBER = 10;
+console.warn("Using excessively high ideology number for debugging");
 
-const IDEOLOGICAL_EDGES = [ ["A", "B"], ["A", "C"], ["B","C"], ["A","D"], ["B","D"], ["C", "D"]];
-
-/*
-	const IDEOLOGICAL_EDGES = [
-		
-		// Generic
-		["Pacifist", "Militarist"],
-		
-		// WTS
-		
-		// Civil war
-		["Religious", "Secular"], 
-		["Socialist", "Fascist"],
-		
-		// Coup
-		["Liberal", "Traditional"],
-		["Liberal", "Radical"],
-		["Radical", "Traditional"],
-		
-		// - economic
-		["Statist", "Keynesian"],
-		["Statist", "Neoliberal"],
-		["Neoliberal", "Keynesian"],
-		
-		// (swingometer is a score not an ideology axis
-		
-		// horizons
-		["Environmentalist", "Industrialist"],
-		
-		// Stellaris, sort of
-		["Democratic", "Authoritarian"],
-		["Xenophobic", "Multicultural"]
+	const IDEOLOGIES = [
+		"Pacifist", "Militarist", "Religious", "Secular", "Socialist", "Fascist", "Liberal", "Traditional", "Radical", 
+			"Statist", "Keynesian", "Neoliberal", "Environmentalist", "Industrialist", "Democratic", "Authoritarian",
+			"Xenophobic", "Multicultural", "Protectionist", "Communist", "Monarchist"
 	];
-*/
 
-
-	
-/*********************************************
-
-		["Liberal", "Traditional"],
-		["Liberal", "Conservative"],
-		["Liberal", "Radical"],
-		["Statist", "Keynesian"],
-		["Statist", "Neoliberal"],
-		["Neoliberal", "Keynesian"],
-		["Religious", "Monarchical"],
-		["Religious", "Atheist"],
-		["Communist", "Fascist"],
-		["Socialist", "Fascist"],
-		["Socialist", "Conservative"],
-		["Communist", "Conservative"],
-		["Pacifist", "Militarist"],
-		["Environmentalist", "Industrialist"],
-		["Redistributionist", "Dedistributionist"],
-		["Democratic", "Authoritarian"],
-	]
-	
-	]
-
-const ALL_IDEOLOGICAL_AXES =  [
-	{"Redistributionist", "Adistributionist", "Dedistributionist"},
-	{"Democratic", "Authoritarian"},
-	{"Divine Right", "Secular Right"},
-	{"Church Primacy"} 
-	]
-*/
+	const IDEOLOGICAL_EDGES = 
+		
+		[
+			// Pacifist
+			["Pacifist", "Militarist"],
+			["Pacifist", "Fascist"],
+			
+			["Militarist", "Environmentalist"],
+			["Militarist", "Monarchist"],
+			
+			// Religious
+			["Religious", "Secular"],
+			["Religious", "Radical"],
+			["Religious", "Democratic"],
+			["Religious", "Authoritarian"],
+			["Religious", "Communist"],
+			["Religious", "Monarchist"],
+			
+			//Secular
+			
+			//Socialist
+			["Socialist", "Fascist"],
+			["Socialist", "Industrialist"],
+			["Socialist", "Authoritarian"],
+			["Socialist", "Communist"],
+			["Socialist", "Monarchist"],
+			
+			// Fascist
+			["Fascist", "Liberal"],
+			["Fascist", "Democratic"],
+			["Fascist", "Multicultural"],
+			["Fascist", "Communist"],
+			["Fascist", "Monarchist"],
+			
+		// TBC
+		];
 	
 const FORBIDDEN_NAMES = ["Watch the Skies", "Urban Nightmare", "A Very British Coup", "A Very British Civil War", "Infinite Horizons", "Blood and Thunder", "Dire Straits", "Hands of the Many"];
+
+const MECHANICALNESSES = ["Political", "Political-Operational", "Operational"];
+
+const MOODS = ["Comedy", "Light-hearted", "Authentic", "Sombre", "Dark"];
 
 /*
 	Faction flavours:	
@@ -145,6 +124,11 @@ const FORBIDDEN_NAMES = ["Watch the Skies", "Urban Nightmare", "A Very British C
 
 				generateIdeologySpaces(megagame);
 				
+				generateMapMechanics(megagame);
+				
+				generateMechanicalness(megagame);
+				
+				generateMood(megagame);
 				
 				$("#mainContent").html(describeMegagame(megagame));
 				
@@ -175,7 +159,7 @@ const FORBIDDEN_NAMES = ["Watch the Skies", "Urban Nightmare", "A Very British C
 				for (refOrCopy of mg.teamTypes)
 				{
 					refOrCopy.cooperationLevel = d(3) - 2;
-					console.log(refOrCopy.name + " are " + refOrCopy.cooperationLevel);
+					// console.log(refOrCopy.name + " are coop-level " + refOrCopy.cooperationLevel);
 				}
 								
 			}
@@ -244,7 +228,7 @@ const FORBIDDEN_NAMES = ["Watch the Skies", "Urban Nightmare", "A Very British C
 							
 							// Check through the possible Y axes for an INDEPENDENT edge
 							for (candidateY of bagOfEdges)
-							{
+							{								
 								console.log("   Considering " + candidateY + " to oppose " + xAxis);
 								
 								if(!xAxis.includes(candidateY[0]) && !xAxis.includes(candidateY[1])) 
@@ -295,18 +279,27 @@ const FORBIDDEN_NAMES = ["Watch the Skies", "Urban Nightmare", "A Very British C
 							
 							for (freeEdgeIndex = 0; freeEdgeIndex < 6; freeEdgeIndex++) {
 							
+								console.log("  Seeking edge to add to polygon after " + freeEdgeIndex);
+							
+
+							
 								var freeEdge = newSpace["edges"][freeEdgeIndex];
+
+								console.log("   free edge is now " + freeEdge);
+
 								var freeNode = freeEdge[1];
-						
-								console.log("  Seeking edge to add to polygon after " + freeEdgeIndex + " (" + freeEdge + ")");
+								
+								console.log("  ([" + freeEdgeIndex + " of " + newSpace["edges"].length + "])");
 						
 								// Keep track as we search of whether we've found what we want...
 								var nextEdgeFound = false;
 								
+								console.log("  Starting to iterate through bag of " + bagOfEdges.length + " edges"); 
+								
 								// Search each remainder in the bag for a suitable edge
 								for (candidateNext of bagOfEdges) {
 									
-									console.log("  Considering " + freeEdge + " <--?--< " + candidateNext );
+									console.log("  Considering " + freeEdge + " <--?--< " + candidateNext + " from a bag of " + bagOfEdges.length);
 									
 									if(candidateNext.includes(freeNode)) {
 									
@@ -344,10 +337,9 @@ const FORBIDDEN_NAMES = ["Watch the Skies", "Urban Nightmare", "A Very British C
 							
 								console.log("  No longer looking for edges to connect");
 							
-								if (nextEdgeFound) {
-									// Time to end the polygon
-									break;
-								}
+								// Time to end the polygon
+								break;
+								
 								
 							// Complete configuring the space
 							
@@ -379,15 +371,56 @@ const FORBIDDEN_NAMES = ["Watch the Skies", "Urban Nightmare", "A Very British C
 				mg.ideologySpaces = ideologySpaces;
 				
 			}// end of GenerateIdeologySpaces
+			
+			function generateMapMechanics(mg) {
+				
+				mg.openMap = coin();
+				
+			}
+			
+			function generateMechanicalness(mg) {
+				
+				mg.mechanicalness = MECHANICALNESSES[d(MECHANICALNESSES.length) - 1]
+				
+			}
+				
+			function generateMood(mg) {
+				
+				mg.mood = MOODS[d(MOODS.length) - 1]
+				
+			}
 				
 			///////////////////////////////////
 			// DATA TO DESCRIPTION FUNCTIONS
 			
 			function describeMegagame(mg) {
 				
+				console.log(mg);
+				
 				var description = "";
 				
 				description += describeSetting(mg);
+				
+				// Play Emphasis & Mood
+				
+				description += "<p>The emphasis of play is " + mg.mechanicalness.toLowerCase() + 
+					", and the mood of the material is " + mg.mood.toLowerCase() + ". ";
+					
+				// Map description
+					
+				var mapDesc = "The game map is ";
+				
+				if (mg.openMap) {
+					mapDesc += "accessible";				
+				}
+				else
+				{
+					mapDesc += "inaccessible";
+				}
+				
+				mapDesc = "<p>" + mapDesc + " to the players.</p>";
+				
+				description += mapDesc;
 				
 				// console.log("There are " + mg.teamTypes.length + " types of team.");
 				
@@ -422,7 +455,7 @@ const FORBIDDEN_NAMES = ["Watch the Skies", "Urban Nightmare", "A Very British C
 			function describeSetting(mg) {
 				var settingDescription = "The era is " 
 				
-				if(mg.isFiction) { settingDescription += "an alternative-history "; }
+				if(mg.isFiction) { settingDescription += "a parallel-world "; }
 				
 				settingDescription += mg.era + x;
 				
@@ -467,7 +500,7 @@ const FORBIDDEN_NAMES = ["Watch the Skies", "Urban Nightmare", "A Very British C
 						
 						var choice = d(drainingArray.length) - 1;
 						
-						console.log(" choice is " + choice);
+						// console.log(" pickFrom() choice is " + choice);
 					
 						fillingArray.push(drainingArray[choice]);
 
@@ -478,9 +511,17 @@ const FORBIDDEN_NAMES = ["Watch the Skies", "Urban Nightmare", "A Very British C
 				}
 				
 				function popRandomFrom(a) {
+					
+					if (popRandomFrom.length < 1) { console.severe("Can't pop from empty array"); return null;} 
+					
+					console.log( "Attempting to pop random from " + a);
+					
 					var choice = d(a.length) - 1;
+					console.log(" Chosen [" + choice + "] of " + a.length + " for random popping");
+					
 					var popped = a[choice];
 					a.splice(choice,1);
+					console.log(" randomly popping " + popped);
 					return popped;
 				}
 				
